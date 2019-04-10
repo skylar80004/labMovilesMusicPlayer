bcb5a094ed14fdea4ed95bbf1b822892cf5e272b
skylar80004 <josedavidvm04@hotmail.com>
2019-04-10 09:38:05 -0600

    Update MainActivity.java

--- MusicPlayer4/app/src/main/java/com/example/musicplayer4/MainActivity.java
+++ MusicPlayer4/app/src/main/java/com/example/musicplayer4/MainActivity.java
@@ -127,6 +127,11 @@ public class MainActivity extends AppCompatActivity {
         seekBarSong.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+                if (fromUser){
+                    this.mediaPlayer.seekTo(progress);
+                       
+                }
 
             }
 
